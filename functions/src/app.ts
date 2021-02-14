b8ce6557618d656aab06b2214f83a161fa74a64e
takapiro99 <55702777+takapiro99@users.noreply.github.com>
2021-02-14 22:02:43 +0900

    got form-data

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -8,31 +8,20 @@ const app: express.Express = express()
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }))
 
-// // CORSの許可
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+const router: express.Router = express.Router();
 
-// if (process.env.NODE_ENV === 'development') {
-//   app.use(express.urlencoded({ extended: true }))
-//   app.use(express.json())
-// }
-// app.use(express.json()) //?
-
-app.get('/', (req, res: express.Response) => {
+router.get('/', (req, res: express.Response) => {
   res.status(200).json({ message: 'hello' })
 })
 
-app.post('/new', newSenryu)
+router.post('/new', newSenryu)
 
-app.use((req, res: express.Response) =>
+router.use((req, res: express.Response) =>
   res.status(404).json({ message: 'no content' })
 )
+
+app.use("/", router)
+
 
 
 export { app }
--- functions/src/newSenryu.ts
+++ functions/src/newSenryu.ts
@@ -1,5 +1,70 @@
 import * as express from 'express'
+import * as Busboy from 'busboy'
+import * as fs from 'fs'
+const os = require('os')
+const path = require('path')
 
-export const newSenryu = (req: express.Request, res: express.Response) => {
-  return res.json({ message: 'done' })
+export const newSenryu = (req: any, res: express.Response) => {
+  const busboy = new Busboy({ headers: req.headers })
+  const tmpdir = os.tmpdir()
+  // This object will accumulate all the fields, keyed by their name
+  const fields: any = {}
+  // This object will accumulate all the uploaded files, keyed by their name.
+  const uploads: any = {}
+  // This code will process each non-file field in the form.
+  busboy.on('field', (fieldname, val) => {
+    // console.log(`Processed field ${fieldname}: ${val}.`)
+    fields[fieldname] = val
+  })
+
+  const fileWrites: any = []
+
+  // This code will process each file uploaded.
+  busboy.on('file', (fieldname, file, filename) => {
+    // console.log(`Processed file ${filename}`)
+    uploads[fieldname] = file
+
+		const filepath = path.join(tmpdir, filename)
+    uploads[fieldname] = filepath
+
+    const writeStream = fs.createWriteStream(filepath);
+    file.pipe(writeStream);
+
+		// 全部書き終わるのを待つ
+		const promise = new Promise((resolve, reject) => {
+      file.on('end', () => {
+        writeStream.end();
+      });
+      writeStream.on('finish', resolve);
+      writeStream.on('error', reject);
+    });
+    fileWrites.push(promise);
+  })
+
+  // Triggered once all uploaded files are processed by Busboy.
+  busboy.on('finish', async () => {
+    await Promise.all(fileWrites)
+		if(!(fields.height && fields.userID && fields.lat && fields.lng)){
+			res.status(422).json({message: "height, userID, lat, lng is required."})
+			busboy.end(req.rawBody)
+			return
+		}
+		if(!(uploads.image1 && uploads.image2 && uploads.image3)){
+			res.status(422).json({message: "image1, image2, image3 is required."})
+			busboy.end(req.rawBody)
+			return
+		}
+		// process file here
+    // fs.readFile(uploads.image1, 'utf-8', (err, data) => {
+    //   // if (err) throw err
+		// 	const buffer = Buffer.from(data, "binary");
+    //   console.log(buffer)
+    // })
+    for (const file in uploads) {
+      fs.unlinkSync(uploads[file])
+    }
+    res.json({ message: 'properly received all data! but not saved in firestore yet' })
+  })
+
+  busboy.end(req.rawBody)
 }
